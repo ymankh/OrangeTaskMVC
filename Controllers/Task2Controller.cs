9f16cdd69c4ef9f6f15f5283b5121b00397c372e
Yaman Khashashneh <ymankh1997@gmail.com>
2024-07-23 23:17:28 +0300

    solve the second task for the MVC

---
+++ Controllers/Task2Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrangeTaskMVC.Controllers
+{
+    public class Task2Controller : Controller
+    {
+        // GET: Task2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(string name, string gender, string email, string phone, string country)
+        {
+            var user = new UserData()
+            {
+                Country = country,
+                Phone = phone,
+                Name = name,
+                Email = email,
+                Gender = gender
+            };
+            ViewBag.User = user;
+            return View();
+        }
+    }
+    public class UserData
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
